ef6f52cf791114c61576384cec560c08167c5d16
Nafil Arzzam <arzzam19@gmail.com>
2022-03-23 12:43:06 +0530

    Meals

--- src/Components/Layout/Header.jsx
+++ src/Components/Layout/Header.jsx
@@ -1,18 +1,50 @@
 import React from "react";
 
 import meals_image from "../../assets/meals.jpeg";
-import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+import styled from "styled-components";
+
+const HeaderStyled = styled.header`
+  position: fixed;
+  box-sizing: border-box;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 5rem;
+  background-color: #8a2b06;
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 10%;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.25);
+  z-index: 10;
+`;
+
+const DivStyled = styled.div`
+  width: 100%;
+  height: 25rem;
+  z-index: 0;
+  overflow: hidden;
+
+  img {
+    width: 110%;
+    height: 100%;
+    object-fit: cover;
+    transform: rotateZ(-5deg) translateY(-4rem) translateX(-1rem);
+  }
+`;
 
 const Header = (props) => {
   return (
     <React.Fragment>
-      <header className={classes.header}>
+      <HeaderStyled className="header">
         <h1>Food Ordering App</h1>
-        <button>Cart</button>
-      </header>
-      <div className={classes["main-image"]}>
+        <HeaderCartButton />
+      </HeaderStyled>
+      <DivStyled>
         <img src={meals_image} alt="meals_image" />
-      </div>
+      </DivStyled>
     </React.Fragment>
   );
 };
---
+++ src/Components/Meals/AvailableMeals.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+import styled from "styled-components";
+
+const Section = styled.section`
+  max-width: 60rem;
+  width: 90%;
+  margin: 2rem auto;
+  animation: meals-appear 1s ease-out forwards;
+
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  @keyframes meals-appear {
+    from {
+      opacity: 0;
+      transform: translateY(3rem);
+    }
+
+    to {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  }
+`;
+
+const DUMMY_MEALS = [
+  {
+    id: "m1",
+    name: "Sushi",
+    description: "Finest fish and veggies",
+    price: 22.99,
+  },
+  {
+    id: "m2",
+    name: "Schnitzel",
+    description: "A german specialty!",
+    price: 16.5,
+  },
+  {
+    id: "m3",
+    name: "Barbecue Burger",
+    description: "American, raw, meaty",
+    price: 12.99,
+  },
+  {
+    id: "m4",
+    name: "Green Bowl",
+    description: "Healthy...and green...",
+    price: 18.99,
+  },
+];
+
+const AvailableMeals = (props) => {
+  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  return (
+    <Section>
+      <ul>{mealsList}</ul>
+    </Section>
+  );
+};
+
+export default AvailableMeals;
