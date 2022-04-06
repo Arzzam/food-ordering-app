e60b047adc27c170e6153181301a8a8d5114c84a
Nafil Arzzam <arzzam19@gmail.com>
2022-04-06 06:59:04 +0530

    Final Touch

--- src/Components/Meals/AvailableMeals.jsx
+++ src/Components/Meals/AvailableMeals.jsx
@@ -34,25 +34,25 @@ const DUMMY_MEALS = [
     id: "m1",
     name: "Sushi",
     description: "Finest fish and veggies",
-    price: 22.99,
+    price: 199.99,
   },
   {
     id: "m2",
     name: "Schnitzel",
     description: "A german specialty!",
-    price: 16.5,
+    price: 250.5,
   },
   {
     id: "m3",
     name: "Barbecue Burger",
     description: "American, raw, meaty",
-    price: 12.99,
+    price: 489.99,
   },
   {
     id: "m4",
     name: "Green Bowl",
     description: "Healthy...and green...",
-    price: 18.99,
+    price: 150.99,
   },
 ];
 
